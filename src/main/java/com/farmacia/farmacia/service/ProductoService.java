5e82f601a334a46e15bc815247bf151b03947867
Kiara <n00386181@upn.pe>
2026-07-03 16:58:07 -0500

    Implementar capa Service para productos

---
+++ src/main/java/com/farmacia/farmacia/service/ProductoService.java
@@ -0,0 +1,17 @@
+package com.farmacia.farmacia.service;
+
+import java.util.List;
+import com.farmacia.farmacia.entity.Producto;
+
+public interface ProductoService {
+
+    List<Producto> listarProductos();
+
+    Producto guardarProducto(Producto producto);
+
+    Producto obtenerProductoPorId(Long id);
+
+    Producto actualizarProducto(Producto producto);
+
+    void eliminarProducto(Long id);
+}
